b617dd776c714e0b20f4d3b3689c2cc20fde35a7
Adrian Nzvimbo <72650425+Jeremaih78@users.noreply.github.com>
2021-07-29 11:59:34 +0200

    Update settings.py

--- dentist/settings.py
+++ dentist/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure--qw$1i(w)3-qm%u=h5l7u2+h3kmq2q0p7#!g9x0m8ix)ri389%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000','aaronmashingaidze.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
